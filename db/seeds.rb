8fa37e7655cb82abf9126ca5251cf1aee54bfe88
James Benjamin <james.abenj@gmail.com>
2020-05-15 04:48:31 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Student.create(first_name: "James", last_name: "Benjamin", active: false)
+Student.create(first_name: "James", last_name: "Benjamin")
